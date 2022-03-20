1f3a69673a60766c0c16f775c1f99fe6e9fe0f49
Priyanka <priyankadombale27@gmail.com>
2022-03-20 15:14:30 +0530

    Added:Exception Handling

---
+++ src/main/java/com/Day21_MoodAnalyzer/MoodAnalysisException.java
@@ -0,0 +1,8 @@
+package com.Day21_MoodAnalyzer;
+
+public class MoodAnalysisException extends Exception {
+
+	public MoodAnalysisException(String msg) {
+		super(msg);
+	}
+}
--- src/test/java/com/Day21_MoodAnalyzer/MoodAnalyserTest.java
+++ src/test/java/com/Day21_MoodAnalyzer/MoodAnalyserTest.java
@@ -3,27 +3,48 @@ package com.Day21_MoodAnalyzer;
 import org.junit.Assert;
 import org.junit.Test;
 
-//Ability to analyse and respond Happy or Sad Mood
-
 public class MoodAnalyserTest {
 	@Test
-	public void givenMessage_WhenSad_ShouldReturnSad() {
+	public void givenMessage_WhenSad_ShouldReturnSad() throws MoodAnalysisException {
 		MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Sad Mood");
-		String mood = moodAnalyser.analyzeMood();
+		String mood = null;
+		mood = moodAnalyser.analyzeMood();
 		Assert.assertEquals("Sad", mood);
 	}
 
 	@Test
 	public void givenMessage_WhenHappy_ShouldReturnHappy() {
 		MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Happy Mood");
-		String mood = moodAnalyser.analyzeMood();
-		Assert.assertEquals("Happy", mood);
+		String mood = null;
+		try {
+			mood = moodAnalyser.analyzeMood();
+			Assert.assertEquals("Happy", mood);
+		} catch (MoodAnalysisException e) {
+			System.out.println(e);
+		}
 	}
 
 	@Test
 	public void givenNullMood_ShouldReturnHappy() {
+		String mood = null;
 		MoodAnalyser moodAnalyser = new MoodAnalyser(null);
-		String mood = moodAnalyser.analyzeMood();
-		Assert.assertEquals("Happy", mood);
+		try {
+			mood = moodAnalyser.analyzeMood();
+			Assert.assertEquals("Happy", mood);
+		} catch (MoodAnalysisException e) {
+			System.out.println(e);
+		}
+	}
+
+	@Test
+	public void givenEmptyMood_ShouldReturnHappy() {
+		String mood = null;
+		MoodAnalyser moodAnalyser = new MoodAnalyser();
+		try {
+			mood = moodAnalyser.analyzeMood();
+			Assert.assertEquals("Happy", mood);
+		} catch (MoodAnalysisException e) {
+			System.out.println(e);
+		}
 	}
 }
